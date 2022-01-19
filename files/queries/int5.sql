17131c6ce3022a94714f93d145408bdb2e654184
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-19 20:15:04 +0000

    Promoção Mais Usada

--- files/queries/int5.sql
+++ files/queries/int5.sql
@@ -2,3 +2,10 @@
 .headers on
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
+select B.nomeP, max(B.numUsos) as numMaxUsos 
+from (select A.nomeP, count(A.nomeP) as numUsos 
+      from (select PessoaCliente.nome as nomeC, Promocao.nome as nomeP 
+            from PessoaCliente, Promocao
+            where Promocao.idPessoaC == PessoaCliente.idPessoaC) as A 
+      group by A.nomeP 
+      order by numUsos DESC) as B;

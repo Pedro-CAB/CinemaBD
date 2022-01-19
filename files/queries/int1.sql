816a16b653432182ddf53744b7eadd25495aa691
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-19 20:15:23 +0000

    Update int1.sql

--- files/queries/int1.sql
+++ files/queries/int1.sql
@@ -2,4 +2,7 @@
 .headers on
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
-select nome, idade, nif from PessoaCliente where idade<23 order by nome ASC;
+select nome, idade, nif
+from PessoaCliente
+where idade<23
+order by nome ASC;

5b3bf35ae9ffb09031f34f150195dea8bb37c0a4
Inês Gaspar <78428139+ines-08@users.noreply.github.com>
2022-01-20 21:19:34 +0000

    Funcionários a trabalhar às 18h

--- files/queries/int7.sql
+++ files/queries/int7.sql
@@ -2,3 +2,8 @@
 .headers on
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
+
+Select nome, funcao
+From PessoaFuncionario, Horario
+Where (PessoaFuncionario.idHorario = Horario.idHorario and entradaH <= '18:00:00' and saidaH >= '18:00:00')
+Order by nome asc;

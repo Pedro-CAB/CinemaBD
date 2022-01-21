61462c94010eda369b80d3c100fe921cfa9ad584
Inês Gaspar <78428139+ines-08@users.noreply.github.com>
2022-01-21 15:25:38 +0000

    Update int8.sql

--- files/queries/int8.sql
+++ files/queries/int8.sql
@@ -7,5 +7,9 @@ PRAGMA foreign_keys = ON;
         select distinct Extras.idBilhete, Extras.oculos3D
         from Extras;
 
-Select sum(oculos3D) as numero_oculos_por_dia, Sessao.data From Extras, Bilhete, Sessao Where (Select data From Sessao, Bilhete Where Bilhete.idSessao = Sessao.idSessao) and 
-Extras.idBilhete = Bilhete.idBilhete and Bilhete.idSessao = Sessao.idSessao Group by Sessao.data;
+Select sum(oculos3D) as numero_oculos_por_dia, Sessao.data 
+From Extras, Bilhete, Sessao 
+Where (Select data 
+       From Sessao, Bilhete 
+       Where Bilhete.idSessao = Sessao.idSessao) and Extras.idBilhete = Bilhete.idBilhete and Bilhete.idSessao = Sessao.idSessao 
+Group by Sessao.data;

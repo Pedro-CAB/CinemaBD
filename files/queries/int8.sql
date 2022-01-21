0b98fe88454beb11a84d8c9927db7ec045fecdd1
Inês Gaspar <78428139+ines-08@users.noreply.github.com>
2022-01-21 15:09:02 +0000

    Update número de óculos comprados em cada dia

--- files/queries/int8.sql
+++ files/queries/int8.sql
@@ -9,3 +9,7 @@ select distinct PessoaCliente.nome, Extras.oculos3D
 from PessoaCliente, Extras 
 where Extras.idPessoaC == PessoaCliente.idPessoaC;
 
+/*
+Select sum(oculos3D), Sessao.data From Extras, Bilhete, Sessao Where (Select data From Sessao, Bilhete Where Bilhete.idSessao = Sessao.idSessao) and 
+Extras.idBilhete = Bilhete.idBilhete and Bilhete.idSessao = Sessao.idSessao Group by Sessao.data;
+*/

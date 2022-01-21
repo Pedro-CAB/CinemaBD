e969c872745bced0f7ff2a01b6a246ebd66e6451
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-21 14:39:26 +0000

    Reformulação Usando Views

--- files/queries/int5.sql
+++ files/queries/int5.sql
@@ -2,10 +2,16 @@
 .headers on
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
+create view A 
+as 
+select PessoaCliente.nome as nomeC, Promocao.nome as nomeP 
+from PessoaCLiente, Promocao 
+where Promocao.idPessoaC == PessoaCliente.idPessoaC;
+
+
+
 select B.nomeP, max(B.numUsos) as numMaxUsos 
 from (select A.nomeP, count(A.nomeP) as numUsos 
-      from (select PessoaCliente.nome as nomeC, Promocao.nome as nomeP 
-            from PessoaCliente, Promocao
-            where Promocao.idPessoaC == PessoaCliente.idPessoaC) as A 
+      from A 
       group by A.nomeP 
       order by numUsos DESC) as B;

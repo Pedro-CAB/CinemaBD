0bbf1286f6105806a28f49ecc0ae51ef5cbf3af8
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-21 15:16:13 +0000

    Filmes do + Assistido ao - Assistido

--- files/queries/int2.sql
+++ files/queries/int2.sql
@@ -2,3 +2,9 @@
 .headers on
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
+
+select distinct Filme.titulo, Filme.genero, Filme.avaliacao, Filme.classEtaria, count(Bilhete.idBilhete) as bilhetesComprados 
+from Filme,Bilhete, Sessao 
+where Filme.idFilme == Sessao.idFilme and Sessao.idSessao == Bilhete.idSessao 
+group by Filme.idFilme 
+order by bilhetesComprados DESC;

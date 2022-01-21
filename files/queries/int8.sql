63040e7292b856d6820367a3ab0f5bea47efd47f
Inês Gaspar <78428139+ines-08@users.noreply.github.com>
2022-01-21 15:16:21 +0000

    Update int8.sql

--- files/queries/int8.sql
+++ files/queries/int8.sql
@@ -3,13 +3,9 @@
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
 
-create view oculosComprados
-as 
-select distinct PessoaCliente.nome, Extras.oculos3D 
-from PessoaCliente, Extras 
-where Extras.idPessoaC == PessoaCliente.idPessoaC;
+ create view oculosComprados as
+        select distinct Extras.idBilhete, Extras.oculos3D
+        from Extras;
 
-/*
 Select sum(oculos3D) as numero_oculos_por_dia, Sessao.data From Extras, Bilhete, Sessao Where (Select data From Sessao, Bilhete Where Bilhete.idSessao = Sessao.idSessao) and 
 Extras.idBilhete = Bilhete.idBilhete and Bilhete.idSessao = Sessao.idSessao Group by Sessao.data;
-*/

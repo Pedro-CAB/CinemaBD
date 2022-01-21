fe811fa22ba2d57069734e65db588f39344a0a8d
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-21 14:53:25 +0000

    Update int8.sql

--- files/queries/int8.sql
+++ files/queries/int8.sql
@@ -2,3 +2,10 @@
 .headers on
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
+
+create view oculosComprados
+as 
+select distinct PessoaCliente.nome, Extras.oculos3D 
+from PessoaCliente, Extras 
+where Extras.idPessoaC == PessoaCliente.idPessoaC;
+

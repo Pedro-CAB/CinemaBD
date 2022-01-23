a9b5a4e4d839a030f54909cdc616ea7fc56501d3
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-23 16:36:05 +0000

    Update int2.sql

--- files/queries/int2.sql
+++ files/queries/int2.sql
@@ -3,7 +3,7 @@
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
 
-select distinct Filme.titulo, Filme.genero, Filme.avaliacao, Filme.classEtaria, count(Bilhete.idBilhete) as bilhetesComprados, sum(Bilhete.preco) as LucroBilhetes, sum(PublicidadePatrocinador.valorPago) as LucroPublicidade 
+select distinct Filme.titulo, Filme.genero, Filme.avaliacao, Filme.classEtaria, count(Bilhete.idBilhete) as bilhetesComprados, sum(Bilhete.preco) as LucroBilhetes, sum(PublicidadePatrocinador.valorPago) as LucroPublicidade, (LucroBilhetes + LucroPublicidade) as LucroTotal 
 from Filme,Bilhete,Sessao, SessaoPublicidade, Publicidade, PublicidadePatrocinador 
 where Filme.idFilme == Sessao.idFilme and Sessao.idSessao == Bilhete.idSessao and Sessao.idSessao == SessaoPublicidade.idSessao and Publicidade.idPublicidade == SessaoPublicidade.idPublicidade and Publicidade.idPublicidade == PublicidadePatrocinador.idPublicidade 
 group by Filme.idFilme 

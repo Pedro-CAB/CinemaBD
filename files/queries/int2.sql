4970286e16b2c2ce9e6b87d4f04b88e1d73ba3b4
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-23 16:38:33 +0000

    Update int2.sql

--- files/queries/int2.sql
+++ files/queries/int2.sql
@@ -3,9 +3,11 @@
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
 
-select distinct Filme.titulo, Filme.genero, Filme.avaliacao, Filme.classEtaria, count(Bilhete.idBilhete) as bilhetesComprados, sum(Bilhete.preco) as LucroBilhetes, sum(PublicidadePatrocinador.valorPago) as LucroPublicidade, (LucroBilhetes + LucroPublicidade) as LucroTotal 
+create view A as 
+select distinct Filme.titulo, Filme.genero, Filme.avaliacao, Filme.classEtaria, count(Bilhete.idBilhete) as bilhetesComprados, sum(Bilhete.preco) as LucroBilhetes, sum(PublicidadePatrocinador.valorPago) as LucroPublicidade 
 from Filme,Bilhete,Sessao, SessaoPublicidade, Publicidade, PublicidadePatrocinador 
 where Filme.idFilme == Sessao.idFilme and Sessao.idSessao == Bilhete.idSessao and Sessao.idSessao == SessaoPublicidade.idSessao and Publicidade.idPublicidade == SessaoPublicidade.idPublicidade and Publicidade.idPublicidade == PublicidadePatrocinador.idPublicidade 
 group by Filme.idFilme 
 order by bilhetesComprados DESC;
 
+select *, (A.LucroBilhetes + A.LucroPublicidade) as LucroTotal from A;

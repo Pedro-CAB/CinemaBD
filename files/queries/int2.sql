c8659a3ade584703ff3430612d51bd1f8cb3a9d0
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-23 16:30:01 +0000

    Update int2.sql

--- files/queries/int2.sql
+++ files/queries/int2.sql
@@ -3,8 +3,9 @@
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
 
-select distinct Filme.titulo, Filme.genero, Filme.avaliacao, Filme.classEtaria, count(Bilhete.idBilhete) as bilhetesComprados 
-from Filme,Bilhete, Sessao 
-where Filme.idFilme == Sessao.idFilme and Sessao.idSessao == Bilhete.idSessao 
+select distinct Filme.titulo, Filme.genero, Filme.avaliacao, Filme.classEtaria, count(Bilhete.idBilhete) as bilhetesComprados, sum(Bilhete.preco) as LucroBilhetes, sum(PublicidadePatrocinador.valorPago) as LucroPublicidade 
+from Filme,Bilhete,Sessao, SessaoPublicidade, Publicidade, PublicidadePatrocinador 
+where Filme.idFilme == Sessao.idFilme and Sessao.idSessao == Bilhete.idSessao and Sessao.idSessao == SessaoPublicidade.idSessao and Publicidade.idPublicidade == SessaoPublicidade.idPublicidade and Publicidade.idPublicidade == PublicidadePatrocinador.idPublicidade 
 group by Filme.idFilme 
 order by bilhetesComprados DESC;
+

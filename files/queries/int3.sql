8ae30081c6e21fd4ffe450744c8a34a6350aa545
Inês Gaspar <78428139+ines-08@users.noreply.github.com>
2022-01-23 18:40:34 +0000

    Change query 3

--- files/queries/int3.sql
+++ files/queries/int3.sql
@@ -3,5 +3,13 @@
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
 
-Select sum(valorPago) as total_publicidade
-From PublicidadePatrocinador;
+Create view Pessoas_Publicidades as 
+Select count(idBilhete) as numero_bilhetes, SessaoPublicidade.idPublicidade, Publicidade.tipo 
+From Bilhete, SessaoPublicidade, Publicidade Where Bilhete.idSessao = SessaoPublicidade.idSessao and SessaoPublicidade.idPublicidade = Publicidade.idPublicidade 
+Group by SessaoPublicidade.idPublicidade;
+
+Select sum(valorPago) as total_publicidade, Patrocinador.nomeEmpresa, Publicidade.tipo, max(valorPago) as maximo_valor_pago, round(max(valorPago)/sum(valorPago) * 100, 2) 
+as percentagem_receita, Pessoas_Publicidades.numero_bilhetes as pessoas_assistem_publicidade_cara
+From PublicidadePatrocinador, Patrocinador, Publicidade, Pessoas_Assistem_Publicidades 
+Where PublicidadePatrocinador.idPatrocinador = Patrocinador.idPatrocinador and PublicidadePatrocinador.idPublicidade = Publicidade.idPublicidade 
+and Publicidade.idPublicidade = Pessoas_Assistem_Publicidades.idPublicidade;

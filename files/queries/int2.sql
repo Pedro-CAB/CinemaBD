926b816830cb8929dbae19dd4e67dfaf65432a68
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-23 17:03:47 +0000

    Update int2.sql

--- files/queries/int2.sql
+++ files/queries/int2.sql
@@ -4,7 +4,7 @@
 PRAGMA foreign_keys = ON;
 
 create view A as 
-select distinct Filme.titulo, Filme.genero, Filme.avaliacao, Filme.classEtaria, count(Bilhete.idBilhete) as bilhetesComprados, sum(Bilhete.preco) as LucroBilhetes, sum(PublicidadePatrocinador.valorPago) as LucroPublicidade 
+select distinct Filme.titulo, Filme.genero, Filme.avaliacao, Filme.classEtaria, count(Bilhete.idBilhete) as numTotalEspectadores, sum(Bilhete.preco) as LucroBilhetes, sum(PublicidadePatrocinador.valorPago) as LucroPublicidade 
 from Filme,Bilhete,Sessao, SessaoPublicidade, Publicidade, PublicidadePatrocinador 
 where Filme.idFilme == Sessao.idFilme and Sessao.idSessao == Bilhete.idSessao and Sessao.idSessao == SessaoPublicidade.idSessao and Publicidade.idPublicidade == SessaoPublicidade.idPublicidade and Publicidade.idPublicidade == PublicidadePatrocinador.idPublicidade 
 group by Filme.idFilme 

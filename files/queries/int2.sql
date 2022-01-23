a24f37fe502cbd695d0ccb24365fe05cf9abbcd0
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-23 17:11:21 +0000

    Update int2.sql

--- files/queries/int2.sql
+++ files/queries/int2.sql
@@ -7,7 +7,8 @@ create view A as
 select distinct Filme.titulo, Filme.genero, Filme.avaliacao, Filme.classEtaria, count(Bilhete.idBilhete) as numTotalEspectadores, sum(Bilhete.preco) as LucroBilhetes, sum(PublicidadePatrocinador.valorPago) as LucroPublicidade 
 from Filme,Bilhete,Sessao, SessaoPublicidade, Publicidade, PublicidadePatrocinador 
 where Filme.idFilme == Sessao.idFilme and Sessao.idSessao == Bilhete.idSessao and Sessao.idSessao == SessaoPublicidade.idSessao and Publicidade.idPublicidade == SessaoPublicidade.idPublicidade and Publicidade.idPublicidade == PublicidadePatrocinador.idPublicidade 
-group by Filme.idFilme 
-order by bilhetesComprados DESC;
+group by Filme.idFilme;
 
-select *, (A.LucroBilhetes + A.LucroPublicidade) as LucroTotal from A;
+select *, (A.LucroBilhetes + A.LucroPublicidade) as LucroTotal 
+from A 
+order by LucroTotal DESC;

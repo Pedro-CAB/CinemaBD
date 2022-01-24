8746f1e7bccf74866ffa744001e58e67ad567b84
Inês Gaspar <78428139+ines-08@users.noreply.github.com>
2022-01-24 17:34:41 +0000

    Update int3.sql

--- files/queries/int3.sql
+++ files/queries/int3.sql
@@ -8,8 +8,15 @@ Select count(idBilhete) as numero_bilhetes, SessaoPublicidade.idPublicidade, Pub
 From Bilhete, SessaoPublicidade, Publicidade Where Bilhete.idSessao = SessaoPublicidade.idSessao and SessaoPublicidade.idPublicidade = Publicidade.idPublicidade 
 Group by SessaoPublicidade.idPublicidade;
 
-Select sum(valorPago) as total_publicidade, Patrocinador.nomeEmpresa, Publicidade.tipo, max(valorPago) as maximo_valor_pago, round(max(valorPago)/sum(valorPago) * 100, 2) 
-as percentagem_receita, Pessoas_Publicidades.numero_bilhetes as pessoas_assistem_publicidade_cara
-From PublicidadePatrocinador, Patrocinador, Publicidade, Pessoas_Publicidades 
-Where PublicidadePatrocinador.idPatrocinador = Patrocinador.idPatrocinador and PublicidadePatrocinador.idPublicidade = Publicidade.idPublicidade 
-and Publicidade.idPublicidade = Pessoas_Publicidades.idPublicidade;
+Create view ValorPublicidades as 
+Select sum(valorPago) as valor, Publicidade.tipo, PublicidadePatrocinador.idPublicidade 
+From PublicidadePatrocinador, SessaoPublicidade, Publicidade 
+Where PublicidadePatrocinador.idPublicidade = SessaoPublicidade.idPublicidade and Publicidade.idPublicidade = SessaoPublicidade.idPublicidade 
+Group by PublicidadePatrocinador.idPublicidade;
+
+Select sum(ValorPublicidades.valor) as total_publicidade, Patrocinador.nomeEmpresa, Publicidade.tipo, max(ValorPublicidades.valor) as 
+maximo_valor_pago, round(max(ValorPublicidades.valor)/sum(ValorPublicidades.valor) *100, 2) as percentagem_max_na_receita, Pessoas_Publicidades.numero_bilhetes 
+as assistencia_publicidade_mais_cara 
+From PublicidadePatrocinador, Patrocinador, Publicidade, Pessoas_Publicidades, ValorPublicidades 
+Where PublicidadePatrocinador.idPatrocinador  =Patrocinador.idPatrocinador and PublicidadePatrocinador.idPublicidade = Publicidade.idPublicidade and 
+ValorPublicidades.idPublicidade = Publicidade.idPublicidade and Publicidade.idPublicidade = Pessoas_Publicidades.idPublicidade;

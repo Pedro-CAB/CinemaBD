60d14580129a105caa94d272d9789a36c773cc32
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-24 17:15:44 +0000

    Update int9.sql

--- files/queries/int9.sql
+++ files/queries/int9.sql
@@ -2,3 +2,13 @@
 .headers on
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
+
+create view A as 
+select distinct Sala.idSala as numSala, Sala.tipoSala as tipo,  Sala.idPessoaF, PessoaFuncionario.nome as funcionarioResponsavel 
+from Sala, PessoaFuncionario, Horario 
+where Sala.idPessoaF = PessoaFuncionario.idPessoaF and PessoaFuncionario.idHorario = Horario.idHorario and Horario.entradaH <= '17:00:00' and Horario.saidaH >= '17:00:00';
+
+create view B as 
+select distinct numSala, tipo, funcionarioResponsavel, Filme.titulo 
+from A, Sessao, Filme 
+where A.numSala = Sessao.sala and Sessao.idFilme = Filme.idFilme and Sessao.inicioH < '17:00:00';

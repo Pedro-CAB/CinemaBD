00343cd0ce60ca056c0ddf597777c4ca432c2302
Inês Gaspar <78428139+ines-08@users.noreply.github.com>
2022-01-24 17:01:13 +0000

    Update int3.sql

--- files/queries/int3.sql
+++ files/queries/int3.sql
@@ -10,6 +10,6 @@ Group by SessaoPublicidade.idPublicidade;
 
 Select sum(valorPago) as total_publicidade, Patrocinador.nomeEmpresa, Publicidade.tipo, max(valorPago) as maximo_valor_pago, round(max(valorPago)/sum(valorPago) * 100, 2) 
 as percentagem_receita, Pessoas_Publicidades.numero_bilhetes as pessoas_assistem_publicidade_cara
-From PublicidadePatrocinador, Patrocinador, Publicidade, Pessoas_Assistem_Publicidades 
+From PublicidadePatrocinador, Patrocinador, Publicidade, Pessoas_Publicidades 
 Where PublicidadePatrocinador.idPatrocinador = Patrocinador.idPatrocinador and PublicidadePatrocinador.idPublicidade = Publicidade.idPublicidade 
-and Publicidade.idPublicidade = Pessoas_Assistem_Publicidades.idPublicidade;
+and Publicidade.idPublicidade = Pessoas_Publicidades.idPublicidade;

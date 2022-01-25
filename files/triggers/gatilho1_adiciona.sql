115e76a6f0bcbf5cefc5a9da9f3fd4f1c21d7cae
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-25 12:29:14 +0000

    Update gatilho1_adiciona.sql

--- files/triggers/gatilho1_adiciona.sql
+++ files/triggers/gatilho1_adiciona.sql
@@ -2,3 +2,10 @@
 .headers on
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
+
+create trigger checkNewBilhete
+before insert on Bilhete
+when exists(select * from Bilhete where idSessao = New.idSessao and numLugar = New.numLugar)
+begin
+select raise(abort,'Lugar Ocupado. Por favor, selecione outro.');
+end;

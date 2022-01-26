5385354345c5b0b3e679100afad1c208a5dd0e8a
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-26 15:09:39 +0000

    Update gatilho2_adiciona.sql

--- files/triggers/gatilho2_adiciona.sql
+++ files/triggers/gatilho2_adiciona.sql
@@ -6,5 +6,5 @@ create trigger checkIdCliente
 before insert on PessoaCliente
 when exists ( select * from PessoaCliente where PessoaCliente.idPessoaC = new.idPessoaC)
 begin
-insert into PessoaCliente values(PessoaCliente.idPessoaC+1, new.nome, new.nif, new.morada , new.telefone, new.dataDeNascimento, new.idade);
+insert into PessoaCliente values(new.idPessoaC+1, new.nome, new.nif, new.morada , new.telefone, new.dataDeNascimento, new.idade);
 end;

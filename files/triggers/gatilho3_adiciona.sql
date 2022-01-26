9774a7c46212732b9a28b930e93a936ff2505823
Pedro Macedo <72477616+pedronunomacedo@users.noreply.github.com>
2022-01-26 15:27:44 +0000

    Update gatilho3_adiciona.sql

--- files/triggers/gatilho3_adiciona.sql
+++ files/triggers/gatilho3_adiciona.sql
@@ -2,3 +2,10 @@
 .headers on
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
+
+create trigger checkSessao
+before insert on Sessao
+when exists (select * from Sessao where inicioH = new.inicioH and data = new.data and sala = new.sala)
+begin
+select raise(abort,'Sala já ocupada com outra sessão. Por favor, selecione outra hora.');
+end;

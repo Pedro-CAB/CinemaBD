b98f0ee8f97e96674e527b664bbfec2ee7e4e93e
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-26 16:40:51 +0000

    Update gatilho1_adiciona.sql

--- files/triggers/gatilho1_adiciona.sql
+++ files/triggers/gatilho1_adiciona.sql
@@ -8,5 +8,5 @@ before insert on Bilhete
 when exists(select * from Bilhete where idSessao = New.idSessao and numLugar = New.numLugar)
 begin
 select raise(abort,'Lugar Ocupado. Por favor, selecione outro.');
-insert into Horario values (new.idBilhete, new.preco, new.numLugar + 1, new.idSessao);
+insert into Bilhete values (new.idBilhete, new.preco, new.numLugar + 1, new.idSessao);
 end;

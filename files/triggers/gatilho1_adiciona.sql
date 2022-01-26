da12d8e21d13f89314ff5178db217527eea72e31
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-26 16:42:34 +0000

    Update gatilho1_adiciona.sql

--- files/triggers/gatilho1_adiciona.sql
+++ files/triggers/gatilho1_adiciona.sql
@@ -7,6 +7,5 @@ create trigger checkNewBilhete
 before insert on Bilhete
 when exists(select * from Bilhete where idSessao = New.idSessao and numLugar = New.numLugar)
 begin
-select raise('Lugar Ocupado. Por favor, selecione outro.');
 insert into Bilhete values (new.idBilhete, new.preco, new.numLugar + 1, new.idSessao);
 end;

336bc2387ed0e0f3398a57f12c6ab2d442ca93ee
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-26 16:23:07 +0000

    Update gatilho2_adiciona.sql

--- files/triggers/gatilho2_adiciona.sql
+++ files/triggers/gatilho2_adiciona.sql
@@ -2,12 +2,3 @@
 .headers on
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
-
-create trigger correctHorarios
-after insert on Horario
-when (time(new.saidaH-new.entradaH)) <> time('08:00:00')
-begin
-update Horario 
-set saidaH = time(new.entradaH) + time('08:00:00')
-where saidaH = time(new.saidaH);
-end;

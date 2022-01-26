2c1281b8f220f953a64930c0785879b5504c1f6b
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-26 15:10:33 +0000

    Update gatilho2_adiciona.sql

--- files/triggers/gatilho2_adiciona.sql
+++ files/triggers/gatilho2_adiciona.sql
@@ -1,10 +1,1 @@
-.mode columns
-.headers on
-.nullvalue NULL
-PRAGMA foreign_keys = ON;
-create trigger checkIdCliente
-before insert on PessoaCliente
-when exists ( select * from PessoaCliente where PessoaCliente.idPessoaC = new.idPessoaC)
-begin
-insert into PessoaCliente values(new.idPessoaC+1, new.nome, new.nif, new.morada , new.telefone, new.dataDeNascimento, new.idade);
-end;
+

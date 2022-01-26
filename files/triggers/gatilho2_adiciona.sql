f1029d3f722547ab903305e26038f533569b8db7
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-26 15:45:55 +0000

    Update gatilho2_adiciona.sql

--- files/triggers/gatilho2_adiciona.sql
+++ files/triggers/gatilho2_adiciona.sql
@@ -7,5 +7,6 @@ create trigger correctHorarios
 before insert on Horario
 when (new.saidaH-new.entradaH) <> '08:00:00'
 begin
+Select raise(ignore);
 insert into Horario values (new.idHorario, new.entradaH, (new.entradaH + '08:00:00'));
 end;

f3841c0b939ec3c43109d0aa957893acc9b676ec
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-26 15:51:21 +0000

    Update gatilho2_adiciona.sql

--- files/triggers/gatilho2_adiciona.sql
+++ files/triggers/gatilho2_adiciona.sql
@@ -8,6 +8,6 @@ after insert on Horario
 when (new.saidaH-new.entradaH) <> '08:00:00'
 begin
 update Horario 
-set saidaH = old.entradaH + '08:00:00' 
+set saidaH = new.entradaH + '08:00:00' 
 where saidaH = old.saidaH;
 end;

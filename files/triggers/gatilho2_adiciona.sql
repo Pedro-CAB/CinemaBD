ed519b5d1786cb77d6649f06c45d71b1f6ff614d
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-26 15:18:10 +0000

    Update gatilho2_adiciona.sql

--- files/triggers/gatilho2_adiciona.sql
+++ files/triggers/gatilho2_adiciona.sql
@@ -3,3 +3,9 @@
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
 
+create trigger correctHorarios
+before insert on Horario
+when (new.saidaH-new.entradaH) <> '08:00:00';
+begin
+insert into Horario values (new.entradaH, (new.entradaH + '08:00:00');
+end;

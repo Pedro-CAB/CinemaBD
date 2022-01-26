f88e445335afaf2d45c0665d6aa4674998c75bcd
Pedro-CAB <80784137+Pedro-CAB@users.noreply.github.com>
2022-01-26 16:26:35 +0000

    Update gatilho2_adiciona.sql

--- files/triggers/gatilho2_adiciona.sql
+++ files/triggers/gatilho2_adiciona.sql
@@ -2,3 +2,10 @@
 .headers on
 .nullvalue NULL
 PRAGMA foreign_keys = ON;
+
+create trigger compareHorarios
+before insert on Horario
+when exists(select * from Horario where Horario.entradaH = new.entradaH and Horario.saidaH = new.saidaH)
+begin
+select raise(abort,'Esse horário já existe.');
+end;
